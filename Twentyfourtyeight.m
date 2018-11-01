2cf3df62afbe5e1efaacfa4447564316bb79dacb
juliogarcia2018 <44072575+juliogarcia2018@users.noreply.github.com>
2018-11-01 13:52:58 -0500

    Add files via upload

--- Twentyfourtyeight.m
+++ Twentyfourtyeight.m
@@ -1,15 +1,27 @@
-% Board will be a 4x4 matrix of zeros
 clc,clear
 
 
-Board = zeros(4)
+Board = zeros(4);
 
 newnumber = randi(10);
-dos = 2;
-cuatro = 4;
 
 if newnumber <= 5
-     Board(randi(numel(Board))) = 2;
+     Board(randi(numel(Board))) = 2
 elseif newnumber > 5 
-    Board(randi(numel(Board))) = 4;
+    Board(randi(numel(Board))) = 4
 end 
+
+newnumber2 = randi(10);
+if newnumber2 <= 5 
+    if Board(randi(numel(Board))) == 0
+     Board(randi(numel(Board))) = 2
+    else Board(randi(numel(Board))) > 0
+        msgbox('I NEED HELP')      % not sure how to get it to re run the if statement until it finds a spot where there is no integer >0 while loop??
+    end
+elseif newnumber2 > 5 & Board(randi(numel(Board))) == 0
+    if Board(randi(numel(Board))) == 0 
+    Board(randi(numel(Board))) = 4
+    else Board(randi(numel(Board))) >0
+        msgbox('I NEED HELP')      % not sure how to get it to re run the if statement until it finds a spot where there is no integer >0 while loop??
+    end
+end
